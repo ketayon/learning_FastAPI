ee909c23ed739f77e297007a0cb0b6d03992334d
Husarov Maksym <ketayon@gmail.com>
2021-12-28 13:55:03 +0200

    Added dynamic routing

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': {'name': 'Max'}}
+    return {'data': 'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data': {'about page'}}
+@app.get('/blog/{id}')
+def show(id):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}

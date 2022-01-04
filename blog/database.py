f3ec0915a9b67ad1b9d560b1dd434f6dd1460e50
Husarov Maksym <ketayon@gmail.com>
2022-01-04 13:46:06 +0200

    Created table at db

--- blog/database.py
+++ blog/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:/// ./blog.db:'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db:'
 
-engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={'check_some_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 

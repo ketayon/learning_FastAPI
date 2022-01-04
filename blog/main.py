3bccf8ee5bd6f2edc8388686b123d6c00fd7de51
Husarov Maksym <ketayon@gmail.com>
2022-01-04 17:42:15 +0200

    Added update method

--- blog/main.py
+++ blog/main.py
@@ -27,9 +27,21 @@ def create(request: schemas.Blog, db:Session=Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db:Session=Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not found')
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'Done'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not found')
+    blog.update(request)
+    db.commit()
+    return 'Updated.'
 
 
 @app.get('/blog')

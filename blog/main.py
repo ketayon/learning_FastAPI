98277860674a2e30e9b52b1350a7f5a86f1f36e3
Husarov Maksym <ketayon@gmail.com>
2022-01-04 17:59:18 +0200

    Added Response model

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, status, Response, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.sql.expression import delete
@@ -44,13 +45,13 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'Updated.'
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all_blogs(db:Session=Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
+from sqlalchemy import orm
 
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class ShowBlog(Blog):
+
+    class Config():
+        orm_mode = True

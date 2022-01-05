e8bc30deca41764808f29586794997f8e414d203
Husarov Maksym <ketayon@gmail.com>
2022-01-05 10:37:08 +0200

    Added hashing of password

--- blog/main.py
+++ blog/main.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql.expression import delete
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -63,7 +64,9 @@ def show(id, response: Response, db:Session=Depends(get_db)):
 
 @app.post('/user')
 def create_user(request: schemas.User, db:Session=Depends(get_db)):
-    new_user = models.User(name = request.name, email = request.email, password = request.password)
+    new_user = models.User(name = request.name,
+                            email = request.email,
+                            password = Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)

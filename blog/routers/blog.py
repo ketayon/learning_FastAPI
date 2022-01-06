8e368cd536d4b44965282051ecf8965fdbf955e2
Husarov Maksym <ketayon@gmail.com>
2022-01-06 13:37:11 +0200

    Added authentication

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, status, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
-from .. import schemas, models, database
+
+from blog.oath2 import get_current_user
+from .. import schemas, models, database, oath2
 from ..repository import blog
 from typing import List
 
@@ -13,23 +15,23 @@ router = APIRouter(
 get_db = database.get_db
 
 @router.get('/', response_model=List[schemas.ShowBlog])
-def all_blogs(db:Session=Depends(get_db)):
+def all_blogs(db:Session=Depends(get_db), current_user: schemas.User = Depends(oath2.get_current_user)):
     return blog.get_all(db)
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db:Session=Depends(get_db)):
+def create(request: schemas.Blog, db:Session=Depends(get_db), current_user: schemas.User = Depends(oath2.get_current_user)):
     blog.create(request, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db:Session=Depends(get_db)):
+def destroy(id: int, db:Session=Depends(get_db), current_user: schemas.User = Depends(oath2.get_current_user)):
     return blog.destroy(id, db)
     
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oath2.get_current_user)):
     return blog.update(id, request, db)
 
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
-def show(id: int, db:Session=Depends(get_db)):
+def show(id: int, db:Session=Depends(get_db), current_user: schemas.User = Depends(oath2.get_current_user)):
     return blog.show(id, db)
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from sqlalchemy import orm
-from typing import List
+from typing import List, Optional
 
 class BlogBase(BaseModel):
     title: str
@@ -31,3 +31,16 @@ class ShowBlog(BaseModel):
 
     class Config():
         orm_mode = True
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
